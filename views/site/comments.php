564af2adb0767beb16e72832fe802f5757b29e27
manservell <chappi@i.ua>
2016-12-08 21:00:35 +0200

    ссылки

--- views/site/comments.php
+++ views/site/comments.php
@@ -4,7 +4,7 @@
 <h1>Коментарии</h1>
 <ul>
 <?php foreach ($comments as $comment){?>
-<li><b><?=$comment->name?>:</b><?=$comment->text?></li>
+<li><b><a href="<?=Yii::$app->urlManager->createUrl(['site/user', 'name'=> $comment->name])?>"><?=$comment->name?>:</b><?=$comment->text?></li>
 <?php } ?>
 </ul>
 <?= LinkPager::widget(['pagination'=>$pagination])?>
